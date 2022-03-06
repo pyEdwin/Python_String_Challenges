31bb405733f34b6e5a8bd81a65f0359729049276
Yapnounkie <edmundopierre@yahoo.com>
2022-03-05 23:30:08 -0800

    solution of count_a_letter

---
+++ count_a_letter.py
@@ -0,0 +1,9 @@
+""" 
+    We are given a string, we are ask to count how many times a character appears in that string.
+"""
+
+my_string = "Hello, I am here to help."
+
+my_string_count = my_string.count('e')
+
+print(my_string_count) # 3
