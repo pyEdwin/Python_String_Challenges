1a5c4066f3fc91ea7ba01ec9bb32f850283b8e6d
Yapnounkie <edmundopierre@yahoo.com>
2022-03-06 00:10:25 -0800

    solution of reverse_string

---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+'''
+     Description: For this challenge you will be reversing a string.
+'''
+
+# using slicing with -1 as a step 
+
+variable = " I speak French"
+
+inverse_variable = variable[: : -1]
